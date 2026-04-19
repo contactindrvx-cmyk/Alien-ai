db38cff74eea858c258187ad8e1a9c76d65d64bf
contactindrvx-cmyk <contact.indrvx@gmail.com>
2026-04-19 19:19:41 +0500

    Update AyeshaAccessibilityService.java

--- app/src/main/java/com/raza/alienai/AyeshaAccessibilityService.java
+++ app/src/main/java/com/raza/alienai/AyeshaAccessibilityService.java
@@ -9,17 +9,15 @@ public class AyeshaAccessibilityService extends AccessibilityService {
     @Override
     public void onServiceConnected() {
         super.onServiceConnected();
-        // جب سروس آن ہوگی تو یوزر کو یہ میسج شو ہوگا
         Toast.makeText(this, "Ayesha AI: Accessibility Ready!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        // 🚀 یہ وہ جگہ ہے جہاں ہم اگلی سٹیج میں عائشہ کو سکرین پڑھنا اور آٹو کلک کرنا سکھائیں گے 🚀
+        // یہاں ہم اگلی سٹیج میں دماغ (Logic) ڈالیں گے
     }
 
     @Override
     public void onInterrupt() {
-        // سروس رکنے پر
     }
 }
